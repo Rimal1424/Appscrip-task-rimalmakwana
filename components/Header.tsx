7c904fccc24334dbe011466a4581b8e5628a58c5
Rimal <127244146+Rimal1424@users.noreply.github.com>
2026-04-04 15:49:23 +0530

    Update Header.tsx

--- components/Header.tsx
+++ components/Header.tsx
@@ -34,7 +34,7 @@ export default function Header() {
 
   return (
     <>
-      {/* ── Top Banner ── */}
+      {/* Top Banner  */}
       <div className="top-banner">
         <div className="top-banner-items">
           <span>Lorem ipsum dolor</span>
